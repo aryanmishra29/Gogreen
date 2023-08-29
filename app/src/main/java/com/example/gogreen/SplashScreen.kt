d178aabec17aefcd26c8bd6443f5562d602ddb52
ajeeshrawal <ajeeshrawal3376@gmail.com>
2023-08-29 23:42:37 +0530

    drwable

---
+++ app/src/main/java/com/example/gogreen/SplashScreen.kt
@@ -0,0 +1,24 @@
+package com.example.gogreen
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        // Delay for 2 seconds and then start the main activity
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginScreen::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000) // 2000 milliseconds
+    }
+}
