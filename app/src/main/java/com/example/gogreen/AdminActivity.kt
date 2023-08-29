dbd33eb14b11bf06953b3625e130f8d7e890b96f
swapnil agrawal <swapnilagrawal259@gmail.com>
2023-08-29 18:16:04 +0530

    update functions

--- app/src/main/java/com/example/gogreen/AdminActivity.kt
+++ app/src/main/java/com/example/gogreen/AdminActivity.kt
@@ -1,11 +1,39 @@
-package com.example.gogreen
+    package com.example.gogreen
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+    import androidx.appcompat.app.AppCompatActivity
+    import android.os.Bundle
+    import androidx.fragment.app.Fragment
+    import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class AdminActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin)
+    class AdminActivity : AppCompatActivity() {
+        private val homeFragment = HomeFragment()
+        private val cartFragment = CartFragment()
+        private val profileFragment = ProfileFragment()
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_admin )
+
+            val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+            // Set the HomeFragment as the initial fragment
+            setCurrentFragment(homeFragment)
+
+            bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.nav_home -> setCurrentFragment(homeFragment)
+                    R.id.nav_cart -> setCurrentFragment(cartFragment)
+                    R.id.nav_profile -> setCurrentFragment(profileFragment)
+                }
+                true
+            }
+        }
+
+        private fun setCurrentFragment(fragment: Fragment) {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragmentContainer, fragment)
+                commit()
+            }
+        }
     }
-}
+
