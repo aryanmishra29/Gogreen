8d238992c30c30a92357fc7adb081f53f621eb77
ajeeshrawal <ajeeshrawal3376@gmail.com>
2023-08-31 23:31:05 +0530

    homescreen changes

--- app/src/main/java/com/example/gogreen/AdminActivity.kt
+++ app/src/main/java/com/example/gogreen/AdminActivity.kt
@@ -2,38 +2,18 @@
 
     import androidx.appcompat.app.AppCompatActivity
     import android.os.Bundle
-    import androidx.fragment.app.Fragment
-    import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
     class AdminActivity : AppCompatActivity() {
-        private val homeFragment = HomeFragment()
-        private val cartFragment = CartFragment()
-        private val profileFragment = ProfileFragment()
+
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_admin )
+            setContentView(R.layout.activity_admin_activity )
 
-            val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
-            // Set the HomeFragment as the initial fragment
-            setCurrentFragment(homeFragment)
-
-            bottomNavigationView.setOnNavigationItemSelectedListener { item ->
-                when (item.itemId) {
-                    R.id.nav_home -> setCurrentFragment(homeFragment)
-                    R.id.nav_cart -> setCurrentFragment(cartFragment)
-                    R.id.nav_profile -> setCurrentFragment(profileFragment)
-                }
-                true
-            }
         }
 
-        private fun setCurrentFragment(fragment: Fragment) {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.fragmentContainer, fragment)
-                commit()
-            }
-        }
+
     }
 
