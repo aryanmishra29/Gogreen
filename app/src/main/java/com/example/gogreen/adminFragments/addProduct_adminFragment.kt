fb4123b0f02016c305224c06805565848daf1117
Bhoomi0207 <bhoomiag0207@gmail.com>
2023-09-01 21:54:39 +0530

    products_add_successfully

--- app/src/main/java/com/example/gogreen/adminFragments/addProduct_adminFragment.kt
+++ app/src/main/java/com/example/gogreen/adminFragments/addProduct_adminFragment.kt
@@ -5,22 +5,30 @@ import android.app.Dialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.gogreen.Category
 import com.example.gogreen.R
 import com.example.gogreen.admin_Adapter.CategoryAdapter
 import com.example.gogreen.admin_Adapter.addProductImageAdapter
+import com.example.gogreen.admin_Model.addProductModel
 import com.example.gogreen.admin_Model.category_Model
 import com.example.gogreen.databinding.FragmentAddProductAdminBinding
 import com.example.gogreen.databinding.FragmentProductAdminBinding
 import com.example.gogreen.databinding.ImageItemBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class addProduct_adminFragment : Fragment() {
@@ -78,10 +86,113 @@ class addProduct_adminFragment : Fragment() {
         adapter =addProductImageAdapter(list)
         binding.productImgRecyclerView.adapter = adapter
 
+        binding.submitButton.setOnClickListener{
+            validateData()
+        }
+
         setProductrCategory()
         return binding.root
     }
-       private fun setProductrCategory(){
+
+    private fun validateData() {
+       if (binding.Productname.text.toString().isEmpty()){
+           binding.Productname.requestFocus()
+           binding.Productname.error= "Empty"
+       }else if (binding.Productprice.text.toString().isEmpty()){
+           binding.Productprice.error = "Empty"
+       }else if (coverImage== null){
+           Toast.makeText(requireContext(),"Please select cover image",Toast.LENGTH_SHORT).show()
+       }else if (list.size<1){
+           Toast.makeText(requireContext(),"please relect product images",Toast.LENGTH_SHORT).show()
+       }else{
+           uploadImage()
+       }
+    }
+
+    private fun uploadImage() {
+        dialog.show()
+
+        val fileName = UUID.randomUUID().toString() + ".jpg"
+
+        val refStorage = FirebaseStorage.getInstance().reference.child("Products/$fileName")
+        refStorage.putFile(coverImage!!)
+            .addOnSuccessListener {
+                it.storage.downloadUrl.addOnSuccessListener { image ->
+                coverImgUrl = image.toString()
+                    uploadProductImage()
+                }
+            }
+            .addOnFailureListener {
+                dialog.dismiss()
+                Toast.makeText(requireContext(), "something went wrong", Toast.LENGTH_SHORT).show()
+
+
+            }
+    }
+     private var i =0
+    private fun uploadProductImage() {
+        dialog.show()
+
+        val fileName = UUID.randomUUID().toString() + ".jpg"
+
+        val refStorage = FirebaseStorage.getInstance().reference.child("Products/$fileName")
+        refStorage.putFile(list[i])
+            .addOnSuccessListener {
+                it.storage.downloadUrl.addOnSuccessListener { image ->
+                    listImages.add(image!!.toString())
+                    if(list.size == listImages.size){
+                        storeData()
+                    }else{
+                        i++
+                        uploadProductImage()
+                    }
+                }
+            }
+            .addOnFailureListener {
+                dialog.dismiss()
+                Toast.makeText(requireContext(), "something went wrong", Toast.LENGTH_SHORT).show()
+
+
+            }
+
+    }
+
+    private fun storeData() {
+        val db = Firebase.firestore.collection("Products")
+                val key = db.id
+
+               val data = addProductModel(
+                 binding.Productname.text.toString(),
+                 binding.Productdesc.text.toString(),
+                 coverImgUrl.toString(),
+                 categoryList[binding.addProductDropdown.selectedItemPosition],
+                 key,
+                 binding.Productprice.text.toString(),
+                 listImages
+               )
+                 db.document(key).set(data).addOnSuccessListener {
+                dialog.dismiss()
+
+                Toast.makeText(requireContext(), "Product added", Toast.LENGTH_SHORT).show()
+                     binding.Productname.text = null
+            }
+            .addOnFailureListener { e ->
+                dialog.dismiss()
+                Log.e("FirebaseError", "Error adding category", e)
+                Toast.makeText(
+                    requireContext(),
+                    "Something went wrong: ${e.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+
+
+    }
+
+
+
+    private fun setProductrCategory(){
            categoryList = ArrayList()
            Firebase.firestore.collection("cate")
                .get().addOnSuccessListener {
