0d3eca236b65e541b3c85a414686880714a8a77d
Bhoomi0207 <bhoomiag0207@gmail.com>
2023-09-01 16:46:47 +0530

    category admin adapter

--- app/src/main/java/com/example/gogreen/adminFragments/Category_adminFragment.kt
+++ app/src/main/java/com/example/gogreen/adminFragments/Category_adminFragment.kt
@@ -1,32 +1,39 @@
 package com.example.gogreen.adminFragments
 
+import android.app.Activity
+import android.app.Dialog
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.gogreen.R
+import com.example.gogreen.admin_Adapter.CategoryAdapter
+import com.example.gogreen.admin_Model.category_Model
+import com.example.gogreen.databinding.FragmentCategoryAdminBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import java.util.*
+import kotlin.collections.ArrayList
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Category_adminFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Category_adminFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1:String? = null
-    private var param2:String? = null
+    private lateinit var binding:FragmentCategoryAdminBinding
+    private var imageUrl:Uri? = null
+    private lateinit var dialog:Dialog
 
-    override fun onCreate(savedInstanceState:Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
+    private var launchGalleryActivity = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            imageUrl = it.data!!.data
+            binding.imageView.setImageURI(imageUrl)
         }
     }
 
@@ -35,26 +42,105 @@ class Category_adminFragment : Fragment() {
         savedInstanceState:Bundle?,
     ):View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category_admin, container, false)
+        binding = FragmentCategoryAdminBinding.inflate(layoutInflater)
+        dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.prgress_layout)
+        dialog.setCancelable(false)
+
+
+      //  getData()
+
+        binding.apply {
+            imageView.setOnClickListener {
+                val intent = Intent("android.intent.action.GET_CONTENT")
+                intent.type = "image/*"
+                launchGalleryActivity.launch(intent)
+            }
+            button.setOnClickListener {
+
+                validateData(binding.categry.text.toString())
+
+            }
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Category_adminFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1:String, param2:String) =
-            Category_adminFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+
+  /*  private fun getData() {
+        val list = ArrayList<category_Model>()
+        Firebase.firestore.collection("cate")
+            .get().addOnSuccessListener {
+                list.clear()
+                for (doc in it.documents){
+                    val data = doc.toObject(category_Model::class.java)
+                    list.add(data!!)
+                }
+                binding.categryRecycler.adapter= CategoryAdapter(requireContext(),list)
+            }
+    }*/
+
+    private fun validateData(categry:String) {
+        if (categry.isEmpty()) {
+            Toast.makeText(requireContext(), "please provide category name", Toast.LENGTH_SHORT)
+                .show()
+        } else if (imageUrl == null) {
+            Toast.makeText(requireContext(), "please select image", Toast.LENGTH_SHORT).show()
+
+        } else {
+            uploadImage(categry)
+        }
+
+    }
+
+    private fun uploadImage(categry:String) {
+        dialog.show()
+
+        val fileName = UUID.randomUUID().toString() + ".jpg"
+
+        val refStorage = FirebaseStorage.getInstance().reference.child("categry/$fileName")
+        refStorage.putFile(imageUrl!!)
+            .addOnSuccessListener {
+                it.storage.downloadUrl.addOnSuccessListener { image ->
+                    storeData(categry, image.toString())
+
                 }
             }
+            .addOnFailureListener {
+                dialog.dismiss()
+                Toast.makeText(requireContext(), "something went wrong", Toast.LENGTH_SHORT).show()
+
+
+            }
+
     }
+
+    private fun storeData(categry:String, Url:String) {
+        val db = Firebase.firestore
+        val data = hashMapOf<String, Any>(
+            "cate" to categry,
+            "image" to Url
+        )
+        db.collection("cate").add(data)
+            .addOnSuccessListener {
+                dialog.dismiss()
+                binding.imageView.setImageDrawable(resources.getDrawable(R.drawable.vector))
+                binding.categry.text = null
+              //  getData()
+                Toast.makeText(requireContext(), "categry updated", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                dialog.dismiss()
+                Toast.makeText(requireContext(), "something went wrong", Toast.LENGTH_SHORT).show()
+
+
+            }
+
+    }
+
+
 }
+
+
+
+
---
+++ app/src/main/java/com/example/gogreen/admin_Model/category_Model.kt
@@ -0,0 +1,6 @@
+package com.example.gogreen.admin_Model
+
+class category_Model (
+    var cate : String? = "",
+    var image :String? = ""
+    )
