55f78b88cf8f35c7bec1114db5c3e2a339ffbbf4
Bhoomi0207 <bhoomiag0207@gmail.com>
2023-09-02 20:13:44 +0530

    roomdb

--- app/src/main/java/com/example/gogreen/User_Activity/ProductDetailActivity.kt
+++ app/src/main/java/com/example/gogreen/User_Activity/ProductDetailActivity.kt
@@ -10,6 +10,7 @@ import com.denzcoskun.imageslider.models.SlideModel
 import com.example.gogreen.R
 import com.example.gogreen.databinding.ActivityProductDetailBinding
 import com.example.gogreen.roomdb.AppDatabase
+import com.example.gogreen.roomdb.ProductDao
 import com.example.gogreen.roomdb.ProductModel
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -22,13 +23,13 @@ class ProductDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityProductDetailBinding.inflate(layoutInflater)
-        getProductDetails(intent.getStringExtra("id"))
+      //  getProductDetails(intent.getStringExtra("id"))
         setContentView(binding.root)
 
 
     }
 
-    private fun getProductDetails(proId:String?) {
+   /* private fun getProductDetails(proId:String?) {
 
         Firebase.firestore.collection("Products")
             .document(proId!!).get().addOnSuccessListener {
@@ -46,6 +47,7 @@ class ProductDetailActivity : AppCompatActivity() {
                 for (data in list) {
                     slidList.add(SlideModel(data, ScaleTypes.CENTER_CROP))
                 }
+                cartAction(proId, name, productSp, it.getString("productCoverImg"))
                 binding.imageSlider.setImageList(slidList)
 
 
@@ -53,5 +55,49 @@ class ProductDetailActivity : AppCompatActivity() {
                 Toast.makeText(this, "Something went wrong", Toast.LENGTH_SHORT).show()
             }
     }
+    private fun cartAction(proId: String, name: String?, productSp: String?, coverimg: String?) {
+
+        val productDao = AppDatabase.getInstance(this).productDao()
+
+        if (productDao.isExit(proId) != null) {
+            binding.textView10.text = "Go to Cart"
+        } else {
+            binding.textView10.text = "Add to Cart"
+        }
+
+        binding.textView10.setOnClickListener {
+            if (productDao.isExit(proId) != null) {
+                openCart()
+            } else {
+                addtoCart(productDao, proId, name, coverimg, productSp)
+            }
+        }
+    }
+
+    private fun addtoCart(
+        productDao:ProductDao,
+        proId: String,
+        name: String?,
+        coverimg: String?,
+        productSp: String?,
+    ) {
+        val data = ProductModel(proId, name, coverimg, productSp)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            productDao.insertProduct(data)
+            binding.textView10.text = "Go to Cart"
+        }
+    }
+
+    private fun openCart() {
+        val preferences = this.getSharedPreferences("info", MODE_PRIVATE)
+        val editor = preferences.edit()
+        editor.putBoolean("isCart", true)
+        editor.apply()
+
+        startActivity(Intent(this, UsersActivity::class.java))
+        finish()
+    }*/
+
 }
 
